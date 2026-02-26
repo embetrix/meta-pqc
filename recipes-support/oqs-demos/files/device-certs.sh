2b79665ec4ad3f614ff9acb2734923a37d7ce790
Ayoub Zaki <ayoub.zaki@embetrix.com>
2026-02-26 12:02:36 +0100

    oqs-demos: device-certs create device server/client certs with extended usage

--- recipes-support/oqs-demos/files/device-certs.sh
+++ recipes-support/oqs-demos/files/device-certs.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# generate pqc/classical device certificate
+# generate pqc/classical device certificates
 # 
 
 # Get device IP
@@ -10,41 +10,75 @@ if ! echo "$DEVICE_IP" | grep -Eq '^([0-9]{1,3}\.){3}[0-9]{1,3}$'; then
 fi
 
 HOSTNAME=$(hostname)
-# Generate self signed PQC device certificate
+# Generate self signed PQC device certificates (CLIENT and SERVER)
 if [ ! -f pqc-ca-cert.pem ] || [ ! -f pqc-ca-key.pem ]; then
     echo "Please generate CA keys first"
     exit 1
 fi
 
-if [ ! -f pqc-device-key.pem ] || [ ! -f pqc-device-cert.pem ]; then
+if [ ! -f pqc-device-server-key.pem ] || [ ! -f pqc-device-server-cert.pem ]; then
 
-    openssl req -new -newkey mldsa65 -keyout pqc-device-key.pem \
-            -out pqc-device-csr.pem -nodes \
+    openssl req -new -newkey mldsa65 -keyout pqc-device-server-key.pem \
+            -out pqc-device-server-csr.pem -nodes \
             -subj "/C=DE/ST=BW/O=Embetrix/OU=PQC-DeviceCert/CN=$HOSTNAME" \
-            -addext "subjectAltName=DNS:$HOSTNAME, DNS:localhost,IP:$DEVICE_IP,IP:127.0.0.1" || exit 1
+            -addext "subjectAltName=DNS:$HOSTNAME, DNS:localhost,IP:$DEVICE_IP,IP:127.0.0.1" \
+            -addext "keyUsage=digitalSignature" \
+            -addext "extendedKeyUsage=serverAuth" || exit 1
 
-    openssl x509 -req -in pqc-device-csr.pem -CA pqc-ca-cert.pem -CAkey pqc-ca-key.pem \
-            -CAcreateserial -days 360 \
-            -out pqc-device-cert.pem \
+    openssl x509 -req -in pqc-device-server-csr.pem -CA pqc-ca-cert.pem -CAkey pqc-ca-key.pem \
+            -CAcreateserial -days 3600 \
+            -out pqc-device-server-cert.pem \
             -copy_extensions copy  || exit 1
 fi
 
+if [ ! -f pqc-device-client-key.pem ] || [ ! -f pqc-device-client-cert.pem ]; then
 
-# Generate self signed RSA device certificate
+    openssl req -new -newkey mldsa65 -keyout pqc-device-client-key.pem \
+            -out pqc-device-client-csr.pem -nodes \
+            -subj "/C=DE/ST=BW/O=Embetrix/OU=PQC-ClientCert/CN=client-$HOSTNAME" \
+            -addext "subjectAltName=DNS:client-$HOSTNAME, DNS:localhost,IP:$DEVICE_IP,IP:127.0.0.1" \
+            -addext "keyUsage=digitalSignature" \
+            -addext "extendedKeyUsage=clientAuth" || exit 1
+
+    openssl x509 -req -in pqc-device-client-csr.pem -CA pqc-ca-cert.pem -CAkey pqc-ca-key.pem \
+            -CAcreateserial -days 3600 \
+            -out pqc-device-client-cert.pem \
+            -copy_extensions copy  || exit 1
+fi
+
+# Generate self signed RSA device certificates (CLIENT and SERVER)
 if [ ! -f ca-cert.pem ] || [ ! -f ca-key.pem ]; then
     echo "Please generate CA keys first"
     exit 1
 fi
 
-if [ ! -f rsa-device-key.pem ] || [ ! -f rsa-device-cert.pem ]; then
+if [ ! -f rsa-device-server-key.pem ] || [ ! -f rsa-device-server-cert.pem ]; then
 
-    openssl req -new -newkey rsa:4096 -keyout rsa-device-key.pem \
-            -out rsa-device-csr.pem -nodes \
+    openssl req -new -newkey rsa:4096 -keyout rsa-device-server-key.pem \
+            -out rsa-device-server-csr.pem -nodes \
             -subj "/C=DE/ST=BW/O=Embetrix/OU=DeviceCert/CN=$HOSTNAME" \
-            -addext "subjectAltName=DNS:$HOSTNAME, DNS:localhost,IP:$DEVICE_IP,IP:127.0.0.1" || exit 1
+            -addext "subjectAltName=DNS:$HOSTNAME, DNS:localhost,IP:$DEVICE_IP,IP:127.0.0.1" \
+            -addext "keyUsage=digitalSignature" \
+            -addext "extendedKeyUsage=serverAuth" || exit 1
 
-    openssl x509 -req -in rsa-device-csr.pem -CA ca-cert.pem -CAkey ca-key.pem \
-            -CAcreateserial -days 360 \
-            -out rsa-device-cert.pem \
+    openssl x509 -req -in rsa-device-server-csr.pem -CA ca-cert.pem -CAkey ca-key.pem \
+            -CAcreateserial -days 3600  \
+            -out rsa-device-server-cert.pem \
+            -copy_extensions copy  || exit 1
+fi
+
+
+if [ ! -f rsa-device-client-key.pem ] || [ ! -f rsa-device-client-cert.pem ]; then
+
+    openssl req -new -newkey rsa:4096 -keyout rsa-device-client-key.pem \
+            -out rsa-device-client-csr.pem -nodes \
+            -subj "/C=DE/ST=BW/O=Embetrix/OU=DeviceCert/CN=client-$HOSTNAME" \
+            -addext "subjectAltName=DNS:client-$HOSTNAME, DNS:localhost,IP:$DEVICE_IP,IP:127.0.0.1" \
+            -addext "keyUsage=digitalSignature" \
+            -addext "extendedKeyUsage=clientAuth" || exit 1
+
+    openssl x509 -req -in rsa-device-client-csr.pem -CA ca-cert.pem -CAkey ca-key.pem \
+            -CAcreateserial -days 3600 \
+            -out rsa-device-client-cert.pem \
             -copy_extensions copy  || exit 1
 fi
